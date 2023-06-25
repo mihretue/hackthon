72928630f89b679535acf08681982e986f5f22d0
wubu <wubeabera123@gmail.com>
2023-06-25 11:20:37 +0300

    wrapping

--- e-commerce/src/components/Login.js
+++ e-commerce/src/components/Login.js
@@ -8,7 +8,7 @@ import {BiSolidUser} from "react-icons/bi";
 //import { Link } from "react-router-dom";
 
 const Login = () =>{
-    const [email,setEmail] = useState('');
+    const [username,setUsername] = useState('');
     const [password,setPassword] = useState('');
     const handleSubmit = (event) =>{
         event.preventDefault();
@@ -16,17 +16,17 @@ const Login = () =>{
     return(
         <div className="div1">
             <div className="container div2 rounded bg-white mx-auto" style={{width:"60%"}}> 
-            <form className="form1 d-block mx-auto" style={{width:"80%"}}>
+            <form className="form1 d-block mx-auto" style={{width:"80%"}} onSubmit={handleSubmit}>
                    <h1 className="text-center fw-bold mt-3 pt-4">Login</h1>
-                   <label for="input1" class="form-label">Email address</label>
+                   <label for="username" class="form-label">Email address</label>
                    <div className="input-group d-flex">
-                      <span className="input-group-text" id="input1" style={{width:"5%"}}><BiSolidUser size="medium"/></span>
-                      <input type="text" class="form-control" id="input1" placeholder="name@gmail.com"/>
+                      <span className="input-group-text text-center" id="username" style={{width:"7%"}}><BiSolidUser/></span>
+                      <input type="text" class="form-control" id="username" placeholder="name@gmail.com" name="username" value={username} onChange={(event) => setUsername(event.target.value)} required/>
                    </div>
-                   <label for="input2" class="form-label mt-3">Password</label>
+                   <label for="password" class="form-label mt-3">Password</label>
                    <div className="input-group d-flex">
-                      <span className="input-group-text" id="input2" style={{width:"5%"}}><PiLockKeyFill/></span>
-                      <input type="password" class="form-control" id="input2" placeholder="password"/>
+                      <span className="input-group-text" id="password" style={{width:"7%"}}><PiLockKeyFill/></span>
+                      <input type="password" class="form-control" id="password" placeholder="password" name="password" value={password} onChange={(event) => setPassword(event.target.value)} required/>
                    </div>
                    <a href="#" class="float-end mt-2" style={{textDecoration:"none",color:"gray",marginRight:"2rem"}}>Forgot password?</a>
                    <button type="submit" class="btn btn-primary d-block mx-auto mt-5" style={{width:"60%"}}>LOGIN</button>
@@ -38,7 +38,8 @@ const Login = () =>{
                    </div>
                    <h6 className="text-center mt-3 pt-1">Or sign up using</h6>
                    <div className="text-center mt-1">
-                   <a href="#" className="" style={{textDecoration:"none",color:"gray"}}>Sign up</a>    </div>
+                   <a href="#" className="" style={{textDecoration:"none",color:"gray"}}>Sign up</a>    
+                   </div>
                   
             </form>
            </div>
